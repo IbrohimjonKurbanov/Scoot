5630cd6598ef3ca3d0c353d1e146ae26170e36c1
ibrohimjonkurbanov <ibrohimjonteamwork@gmail.com>
2023-01-07 20:54:15 +0500

    Scoot

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const headerToggleBtn =document.querySelector('.header__toggle-btn');
+const headerToggleBtnMenu =document.querySelector('#menu');
+const sitenav =document.querySelector('.sitenav');
+const overlay = document.querySelector('.overlay');
+
+headerToggleBtn.addEventListener('click', () => {
+  sitenav.classList.toggle('show-nav');
+  overlay.classList.toggle('show-nav');
+  menu.classList.toggle('bx-x');
+})
+
+    
